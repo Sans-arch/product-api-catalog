6015549a72dc4a7fa1d9a0cd1192a9507e8c6e8e
Sans-arch <sansfleitas@gmail.com>
2026-04-22 21:48:08 -0300

    refactor Redis configuration to use GenericJacksonJsonRedisSerializer and improve object mapping

--- src/main/java/com/github/sansarch/productcatalogapi/infra/RedisConfig.java
+++ src/main/java/com/github/sansarch/productcatalogapi/infra/RedisConfig.java
@@ -7,32 +7,44 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.DefaultTyping;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory(); // reads host/port from application.yaml
-    }
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new JedisConnectionFactory(); // reads host/port from application.yaml
+  }
 
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10)) // cache expires after 10 min
-                .disableCachingNullValues() // don't cache nulls
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer() // store as JSON, not binary
-                        )
-                );
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    var objectMapperSerializer = JsonMapper.builder()
+        .activateDefaultTyping(
+            BasicPolymorphicTypeValidator.builder()
+                .allowIfBaseType(Object.class)
+                .build(),
+            DefaultTyping.NON_FINAL
+        )
+        .build();
 
-        return RedisCacheManager.builder(connectionFactory)
-                .cacheDefaults(config)
-                .build();
-    }
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(10)) // cache expires after 10 min
+        .disableCachingNullValues() // don't cache nulls
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJacksonJsonRedisSerializer(objectMapperSerializer)
+            )
+        );
+
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(config)
+        .build();
+  }
 }
